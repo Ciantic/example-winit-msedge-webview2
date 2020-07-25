a032072bac3d0596da92f4873702de1c1452fd2c
Jari Pennanen <ciantic@oksidi.com>
2020-07-25 23:56:53 +0300

    refactor as library...

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use webviewbuilder::{ReceiveWebviewMessage, WebViewBuilder};
+use webviewbuilder_win::{ReceiveWebviewMessage, WebViewBuilder};
 use winit::event::{Event, WindowEvent};
 use winit::{
     dpi::LogicalSize,
